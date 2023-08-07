b0b962cc18aa30c197f9354b17b286848f7db573
Shara <shara.shahverdian@gmail.com>
2023-08-08 00:03:11 +0400

    add AMD-Swap

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# AMD-Swap
-Effortlessly swap Armenian Dram (AMD) for various global currencies on our user-friendly personal currency exchange website.
+# Personal Currency Exchange Platform
+
+Welcome to our user-friendly personal currency exchange platform, where you can seamlessly convert Armenian Dram (AMD) to a diverse range of global currencies
+
+## Key Features
+
+- **Real-Time Exchange Rates**: Stay up-to-date with real-time exchange rates to make informed decisions about your currency swaps.
+
+- **Wide Currency Selection**: Choose from a variety of global currencies to convert your AMD, tailored to your specific needs.
+
+- **Intuitive Interface**: Our user-friendly interface makes it easy for both beginners and experienced users to navigate and perform exchanges effortlessly.
---
+++ app.js
@@ -0,0 +1,30 @@
+// Get the input and result fields
+const amdInput = document.getElementById('amd-input');
+const usdResult = document.getElementById('usd-result');
+const tomResult = document.getElementById('tom-result');
+
+// Define the initial values
+const amd = 385.96;
+const tom = 49650;
+
+// Add an event listener to the amdInput field to calculate and update the results
+amdInput.addEventListener('input', function() {
+    const amdAmount = parseFloat(amdInput.value.replace(/[^\d.]/g, '')); // Parse the AMD amount input to a number
+    
+    if (isNaN(amdAmount) || amdAmount <= 0) {
+        amdInput.value = '';
+        usdResult.value = '';
+        tomResult.value = '';
+    } else {
+        // Calculate USD and TOM amounts based on the initial values of amd and tom
+        const usdAmount = amdAmount / amd;
+        const tomAmount = Math.ceil(tom * usdAmount);
+        
+        usdResult.value = isNaN(usdAmount) ? '' : '$ ' + usdAmount.toFixed(2); // Display USD amount with 2 decimal places
+        tomResult.value = isNaN(tomAmount) ? '' : 'T ' + tomAmount.toLocaleString(undefined, { minimumFractionDigits: 0 }); // Display TOM amount with 2 decimal places
+        amdInput.value = '֏ ' + amdAmount.toLocaleString(undefined, { minimumFractionDigits: 0 });
+    }
+});
+
+tippy("#amdFlag", {content: amd});
+tippy("#tomFlag", {content: tom.toLocaleString(undefined, { minimumFractionDigits: 0 })});
