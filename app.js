97bdaf5ddff77c8fa6a38d175a59daef9e87a4a1
Shara <shara.shahverdian@gmail.com>
2023-10-14 18:44:18 +0330

    Update price

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const currencyData = [
     { name: 'amd', rate: 396.13, symbol: '֏' },
     { name: 'usd', rate: 1, symbol: '$' },
-    { name: 'tom', rate: 51550, symbol: 'T' },
+    { name: 'tom', rate: 50743, symbol: 'T' },
     { name: 'gel', rate: 2.63, symbol: '₾' },
     { name: 'gbp', rate: 0.82, symbol: '£' },
     { name: 'eur', rate: 0.95, symbol: '€' },
