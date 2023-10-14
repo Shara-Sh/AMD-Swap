1bb9e0206acf1844b163c48750b2e65a6372fcbf
Shara Shahverdian <sharashahverdian@gmail.com>
2023-10-14 20:33:59 +0000

    Update Currency Data

--- price.js
+++ price.js
@@ -1,9 +1,9 @@
 export const currencyData = [
     { name: 'amd', rate: 401.73, symbol: '֏' },
     { name: 'usd', rate: 1, symbol: '$' },
-    { name: 'tom', rate: 50691, symbol: 'T' },
+    { name: 'tom', rate: 50798, symbol: 'T' },
     { name: 'gel', rate: 2.69, symbol: '₾' },
     { name: 'gbp', rate: 0.82, symbol: '£' },
-    { name: 'eur', rate: 0.95, symbol: '€' },
+    { name: 'eur', rate: 0.950, symbol: '€' },
     // Add more currencies here
 ];
