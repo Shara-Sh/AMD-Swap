e54564db5fc25ebd823af8b58ed384bc8c710ec6
Shara <shara.shahverdian@gmail.com>
2023-10-16 19:39:08 +0330

    Update price.py

--- price.py
+++ price.py
@@ -1,3 +1,4 @@
+import math
 import requests
 from bs4 import BeautifulSoup
 
@@ -18,7 +19,8 @@ amdPage = requests.get(amdURL)
 amdSoup = BeautifulSoup(amdPage.content, "html.parser")
 amd_element = amdSoup.find("p", class_="result__BigRate-sc-1bsijpp-1 iGrAod")
 
-amdPrice = amd_element.text.split()[0][:-2]
+amdPrice = amd_element.text.split()[0]
+amdPrice = math.floor(float(amdPrice) * 100) / 100
 
 # USD
 usdPrice = 1
@@ -30,7 +32,8 @@ gelPage = requests.get(gelURL)
 gelSoup = BeautifulSoup(gelPage.content, "html.parser")
 gel_element = gelSoup.find("p", class_="result__BigRate-sc-1bsijpp-1 iGrAod")
 
-gelPrice = gel_element.text.split()[0][:-5]
+gelPrice = gel_element.text.split()[0]
+gelPrice = math.floor(float(gelPrice) * 100) / 100
 
 # GBP
 gbpURL = "https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=GBP"
@@ -39,7 +42,8 @@ gbpPage = requests.get(gbpURL)
 gbpSoup = BeautifulSoup(gbpPage.content, "html.parser")
 gbp_element = gbpSoup.find("p", class_="result__BigRate-sc-1bsijpp-1 iGrAod")
 
-gbpPrice = gbp_element.text.split()[0][:-6]
+gbpPrice = gbp_element.text.split()[0]
+gbpPrice = math.floor(float(gbpPrice) * 100) / 100
 
 # EUR
 eurURL = "https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=EUR"
@@ -48,7 +52,8 @@ eurPage = requests.get(eurURL)
 eurSoup = BeautifulSoup(eurPage.content, "html.parser")
 eur_element = eurSoup.find("p", class_="result__BigRate-sc-1bsijpp-1 iGrAod")
 
-eurPrice = eur_element.text.split()[0][:-6]
+eurPrice = eur_element.text.split()[0]
+eurPrice = math.floor(float(eurPrice) * 100) / 100
 
 # price.js
 js_variable = f"export const currencyData = [\n"
